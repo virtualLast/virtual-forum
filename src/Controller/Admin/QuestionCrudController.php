2b20aace3bc5a5571f118f9dabacef4240eb8248
virtualLast <mjlast@gmail.com>
2022-11-16 21:21:31 +0000

    limited the question index to user results

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -17,6 +23,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class QuestionCrudController extends AbstractCrudController
 {
+//    private EntityRepository $entityRepository;
+//
+//    public function __construct(EntityRepository $entityRepository)
+//    {
+//        $this->entityRepository = $entityRepository;
+//    }
+
     public static function getEntityFqcn(): string
     {
         return Question::class;
@@ -64,5 +77,15 @@ class QuestionCrudController extends AbstractCrudController
             ->add(DateTimeFilter::new('createdAt'))
             ->add(DateTimeFilter::new('updatedAt'))
             ;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $response = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        if(!in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+            $response->andWhere('entity.createdBy = :createdBy')->setParameter('createdBy', $this->getUser());
+        }
+
+        return $response;
     }
 }
