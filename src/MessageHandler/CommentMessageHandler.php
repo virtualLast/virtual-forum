d78bba431ecd1cc1e8d35ac6a978bebaddef3686
virtualLast <mjlast@gmail.com>
2022-11-21 13:10:32 +0000

    added in a basic workflow to the submitting of comments

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -5,18 +5,27 @@ namespace App\MessageHandler;
 use App\Components\SpamChecker;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 final class CommentMessageHandler implements MessageHandlerInterface
 {
     private CommentRepository $commentRepository;
     private SpamChecker $spamChecker;
+    private MessageBusInterface $bus;
+    private WorkflowInterface $workflow;
+    private LoggerInterface $logger;
 
-    public function __construct(CommentRepository $commentRepository, SpamChecker $spamChecker)
+    public function __construct(CommentRepository $commentRepository, SpamChecker $spamChecker, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, LoggerInterface $logger = null)
     {
-        $this->commentRepository = $commentRepository;
-        $this->spamChecker       = $spamChecker;
+        $this->commentRepository    = $commentRepository;
+        $this->spamChecker          = $spamChecker;
+        $this->workflow             = $commentStateMachine;
+        $this->logger               = $logger;
+        $this->bus                  = $bus;
     }
 
     /**
@@ -29,12 +38,27 @@ final class CommentMessageHandler implements MessageHandlerInterface
             return;
         }
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setStatus('spam');
-        } else {
-            $comment->setStatus('published');
+
+        if($this->workflow->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+            
+            if($score === 2) {
+                $transition = 'reject_spam';
+            } elseif ($score === 1) {
+                $transition = 'might_be_spam';
+            }
+
+            $this->workflow->apply($comment, $transition);
+            $this->commentRepository->save($comment, true);
+            $this->bus->dispatch($message);
+        } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+            $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+            $this->commentRepository->save($comment, true);
+        }
+        elseif ($this->logger) {
+            $this->logger->debug('Dropping comment message', ['comment' => $comment->getId(), 'status' => $comment->getStatus()]);
         }
 
-        $this->commentRepository->save($comment, true);
     }
 }
